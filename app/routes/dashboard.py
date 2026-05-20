de237b96f545a1ee646c6c7ea7a4aa243a7262bf
nitikpsn <nitikpsn@gmail.com>
2026-05-21 02:24:18 +0530

    updateing dasboard

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,7 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_required
 
-dashboard = Blueprint('dashboard', __name__, url_prefix='/dashboard')
+dashboard = Blueprint('dashboard', __name__)
 
-@dashboard.route('/')
+@dashboard.route('/dashboard')
+# @login_required  <-- Comment this out for 5 minutes just to test if the page looks good!
 def index():
-    return ''
+    return render_template('dashboard/dashboard.html')
