4699c81f958a1b088200a5b5414331542e9358d6
nitikpsn <nitikpsn@gmail.com>
2026-05-22 10:12:33 +0530

    Implement note creation feature with form handling and UI updates

--- app/forms/note_form.py
+++ app/forms/note_form.py
@@ -3,9 +3,6 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class NoteForm(FlaskForm):
-    title = StringField('Title', validators=[
-        DataRequired(), 
-        Length(min=2, max=200)
-    ])
+    title = StringField('Title', validators=[DataRequired(), Length(max=200)])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Save to Atlas')
--- app/routes/notes.py
+++ app/routes/notes.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_required, current_user
+from app.models.user import db
+from app.models.note import Note
+from app.forms.note_form import NoteForm
 
-notes = Blueprint('notes', __name__, url_prefix='/notes')
+notes = Blueprint('notes', __name__)
 
-@notes.route('/')
-def index():
-    return ''
+@notes.route('/note/new', methods=['GET', 'POST'])
+@login_required
+def create_note():
+    form = NoteForm()
+    if form.validate_on_submit():
+        new_note = Note(
+            title=form.title.data,
+            content=form.content.data,
+            author=current_user
+        )
+        db.session.add(new_note)
+        db.session.commit()
+        flash('Note added to your Atlas!', 'success')
+        return redirect(url_for('dashboard.index'))
+    return render_template('notes/create_note.html', form=form)
