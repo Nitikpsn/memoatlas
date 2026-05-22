c4d1cfd2d912dbe770d5adef66d956b35bc72efa
nitikpsn <nitikpsn@gmail.com>
2026-05-22 09:26:48 +0530

    rewrite

--- run.py
+++ run.py
@@ -2,6 +2,5 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    # debug=True lets the server restart automatically when you change code
+if __name__ == '__main__':
     app.run(debug=True)
